27bd6d8e0fd1be19553275800897cb1a9a7d3d8a
lushy <31211482+lushy@users.noreply.github.com>
2018-05-07 10:14:37 +0800

    修改编码

--- src/main/java/com/zzxy/action/UserController.java
+++ src/main/java/com/zzxy/action/UserController.java
@@ -1,1 +1,34 @@
-package com.zzxy.action;import org.springframework.stereotype.Controller;import org.springframework.ui.ModelMap;import org.springframework.web.bind.annotation.ModelAttribute;import org.springframework.web.bind.annotation.RequestMapping;import com.zzxy.po.User;@Controller@RequestMapping("/user")public class UserController {	/**	 * 保存用户	 * 	 * @param id	 * @return	 */	@RequestMapping("")	public String addUser(ModelMap model) {		return "addUser";	}	/**	 * 保存用户	 * 	 * @param id	 * @return	 */	@RequestMapping("/save")	public String saveUser(ModelMap model, @ModelAttribute("form") User user) {		model.addAttribute("user", user);		return "showUser";	}}
+package com.zzxy.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zzxy.po.User;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	/**
+	 * 淇濆瓨鐢ㄦ埛
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("")
+	public String addUser(ModelMap model) {
+		return "addUser";
+	}
+	/**
+	 * 淇濆瓨鐢ㄦ埛
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/save")
+	public String saveUser(ModelMap model, @ModelAttribute("form") User user) {
+		model.addAttribute("user", user);
+		return "showUser";
+	}
+}
